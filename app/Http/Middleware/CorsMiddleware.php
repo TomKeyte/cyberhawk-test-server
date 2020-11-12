1e5198f9ded07c66d3bf1bb3df43ad745ad40532
thomaskeyte <tom.keyte@merj.com>
2020-11-12 18:46:22 +0000

    Add CORS middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class ExampleMiddleware
+class CorsMiddleware
 {
     /**
      * Handle an incoming request.
@@ -15,6 +15,9 @@ class ExampleMiddleware
      */
     public function handle($request, Closure $next)
     {
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Origin', '*');
+        
         return $next($request);
     }
 }
